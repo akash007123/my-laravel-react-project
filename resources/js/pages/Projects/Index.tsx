0efb67c7a768e10a140c74383f85ca3e916ffea3
Akash <akashraikwar763@gmail.com>
2025-08-30 13:25:10 +0530

    Updated git ignore

--- resources/js/pages/Projects/Index.tsx
+++ resources/js/pages/Projects/Index.tsx
@@ -36,6 +36,7 @@ export default function ProjectsIndex({ projects, allProjects, tab = 'List' }: P
         'bg-pink-100 text-pink-800',
         'bg-violet-100 text-violet-800',
         'bg-neutral-100 text-neutral-800',
+        'bg-neutral-100 text-neutral-800',
     ];
 
     // Close dropdown when clicking outside
